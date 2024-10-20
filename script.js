634f3d870c44af4c178688fe9db82fd60c72fc41
xFadedx <156910285+xFadedx@users.noreply.github.com>
2024-10-20 16:41:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
-    }
-}
-
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-
-
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
-});
-
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
-});
-
-
-// ABOVE IS LINK SMOOTHNESS
-// BELOW IS TYPING EFFECT
-
-
 // Array of different roles to rotate
 const roles = ["Web Developer","Software Developer","Freelancer","Problem Solver"]; 
 
@@ -79,6 +51,7 @@ function toggleMenu() {
     var body = document.body;
     body.classList.toggle("burger-active");
 }
+
 
 
 
